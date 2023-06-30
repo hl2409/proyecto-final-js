c368629b56cce3f613de8e421ac2dccf4f0bd0da
hl2409 <137825582+hl2409@users.noreply.github.com>
2023-06-30 17:40:19 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@ console.log('Central de turnos medicos')
 console.log('--------------');
 
 // sistema de turnos
-/* si hay una calculadora forzada para la rubrica es por que con lo aprendido hasta ese momento, no veia necesidad de usar una iteracion en la base de lo que sera el proyecto final, que debido a mala o falta de ella de los tutores que no aparecen nunca, no dicen nada, no contestan la comunicacion fracasa, 
+/* si hay una calculadora forzada para la rubrica es por que con lo aprendido hasta ese momento, 
+no veia necesidad de usar una iteracion en la base de lo que sera el proyecto final, 
+en todo caso no le llamen primer preentrega y hago ejercicios y cumplo las rubricas con menos esfuerzo, 
+que debido a mala o falta de comunicacion de los tutores uno se siente perdiddo, pero uno solo no, somos un grupo de estudiantes, y bastante grande que esta molesto por las formas, por no explicar, por no responder.
 la segunda preentrega terminara siendo algo hecho completamente de cero, si esta mas que claro que estoy aplicando los conceptos correctamente , y no los estoy forzando donde no los necesito, no podes bajar puntos por que sos un vago. desmotivas.
 */
+// cambian los tutores sin avisar, el tutor no se comunica ni pregunta por que mi trabajo esta atrasado, 
+//estuve 20 dias enfermo todos los dias viendo clases por que estaba atrasado coder me dijo que no me haga problema que si no me cambiaba de comision todo se arreglaba
+// llega uno que ni hola dice te corrige sin comentarios, y te corrige para abajo basandose en un powerpoint.
+
 console.log('Bienvenido a la central de turnos');
 console.log('--------------');
 
